b63ff41dab9b4acbc21694699081c076ee9530f2
Ger Hobbelt <ger@hobbelt.com>
2013-07-14 23:15:47 +0200

    updated the Gruntfile: comments and preliminary tweaks to make grunt build the 'default' target out of the box.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,7 @@
+/* global module:false */
 module.exports = function(grunt) {
+
+  // Project configuration
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
@@ -64,10 +67,15 @@ module.exports = function(grunt) {
     }
   });
 
+  // Dependencies
   grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+  grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+
+  // Default task
+  grunt.registerTask("default", ["jshint", "concat", "uglify"]);
 
   // A convenient task alias.
   grunt.registerTask('test', 'qunit');
-  grunt.registerTask("default", ["jshint", "concat", "uglify"]);
 
 };
